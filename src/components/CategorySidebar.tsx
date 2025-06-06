b28426c95bd5cae990b90fef55737e51b38c25d7
Taoufik-122 <toufikamb@gmail.com>
2025-06-06 17:39:20 +0100

    تحويل جلب البيانات إلى Supabase V5

--- src/components/CategorySidebar.tsx
+++ src/components/CategorySidebar.tsx
@@ -50,7 +50,7 @@ const CategorySidebar = () => {
                   isActive={!category}
                   tooltip="All Products"
                 >
-                  <Link to="/products">
+                  <Link to="">
                     <Package size={20} />
                     <span>All Products</span>
                   </Link>
